9c9938a5764f5de7b7bef5d6a1963fe336e6cf1b
Igor Stinieski Favin <34497491+igorfavin@users.noreply.github.com>
2022-11-26 23:16:57 -0300

    dynamic arithmetic average

--- repeatingStructure.cpp
+++ repeatingStructure.cpp
@@ -7,7 +7,19 @@ using namespace std;
 
 int main()
 {
+    int qtd;
+    float notas, total;
+
     setlocale(LC_ALL, "Portuguese");
-  
+    printf("Quantas notas tem para fazer a média?\n");
+    scanf("%d%*c", &qtd);
+    for(int i = 0; i < qtd; i++)
+    {
+        printf("Digite a nota %dº: ", i+1);
+        scanf("%f%*c", &notas);
+        total += notas;
+    }
+    printf("A média das notas é: %.2f", (total/qtd));
+
     return 0;
 }
